88036b694483a22c2a6f07c6be296e536002df4e
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-10-03 20:56:37 +0300

    Add starter code

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
