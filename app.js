ac4bf660c6d51e73999028e69d1fbd59d97d3528
Basma Sabry <basmasabry33333@gmail.com>
2024-02-21 12:59:13 +0200

    Configure URL routing with Express

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { validateUrlInput } = require('./middlewares/urlMiddleware');
+
+
 
 const indexRouter = require('./routes/index');
 
@@ -12,13 +15,23 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+require('dotenv').config()
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConfig')
+
 
 app.use('/', indexRouter);
+//app.use('/shorten', indexRouter);
+
+
+// Connect to MongoDB
+connectDB();
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+const { validateUrlInput } = require('../middlewares/urlMiddleware');
+const urlController = require('../controllers/urlController');
+
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
+router.post('/shorten', validateUrlInput, urlController.shortenUrl);
+router.get('/:alias', urlController.redirectToOriginalUrl);
+router.get('/:alias/stats', urlController.getUrlStats);
+
 
 module.exports = router;
