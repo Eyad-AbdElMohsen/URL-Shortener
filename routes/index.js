4be7f2767ffa886c2eea8976b4391be140d3287e
SALEH <salehenab850@gmail.com>
2024-05-10 00:00:44 +0300

    Update table based on Databse & Save submitted data to the Database

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
 const router = require('express').Router();
+const URL = require('../models/URL_Model');
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    URL.find()
+        .then((links) => {
+            res.render('index', { title: 'Express', links });
+        })
+        .catch((err) => {
+            res.render('error', { error: "Failed to load home page" })
+        })
+
 });
+
+
+/* Save the data coming from the form in the Database */
+router.post('/', (req, res) => {
+
+    const long = req.body.urlInput
+    const alias = req.body.aliasInput
+    const url = new URL({ long, alias })
+    url.save()
+        .then(() => {
+            res.redirect('/')
+            console.log("Link added successfully to the Database.")
+        })
+        .catch((err) => {
+            res.render('error', { error: "Failed to save data to the database" })
+        })
+})
 
 module.exports = router;
