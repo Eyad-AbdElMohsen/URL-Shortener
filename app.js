87091277ca0ea6931c004ebf524749f804ab0557
EyadMohsen <ahmedeyad84@gmail.com>
2024-05-14 23:54:25 +0300

    Refactoring addUrl.js

--- app.js
+++ app.js
@@ -21,18 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', addUrlRouter);
 app.use('/', homePageRouter);
 
-
-
-
-
-
-
-
-
-
-
-
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
--- routes/addUrl.js
+++ routes/addUrl.js
@@ -8,25 +8,69 @@ const urlPattern = new RegExp('^(https?://)?'+
     '(\\:\\d+)?(/[-a-z\\d%_.~+]*)*'+
     '(\\?[;&a-z\\d%_.~+=-]*)?'+
     '(\\#[-a-z\\d_]*)?$','i');
-const pattern = /[^a-zA-Z0-9_]/;
+const aliasPattern = /[^a-zA-Z0-9_]/;
 
-router.post('/' , async(req , res , next) => {
-    await mongoose.connect(DB_URL);
-    if (!urlPattern.test(req.body.urlInput)){
-        res.status('404').send('Not a valid Email');
+router.post('/',
+    validateUrl,
+    validateAlias,
+    saveData,
+    updateData
+);
+
+// URL check
+function validateUrl(req, res, next) {
+    if (!urlPattern.test(req.body.urlInput)) {
+        res.status(400).send('Not a valid URL');
+    } else {
+        console.log(1);
+        next();
     }
-    if (pattern.test(req.body.aliasInput)) {
-        res.status('404').send('Alias contains invalid characters');
-    }
-    else{
-    let newUrl = new Url({
-    url : req.body.urlInput,
-    alias : req.body.aliasInput,
-    })
-    let url = await newUrl.save();
-    console.log(url);
-    mongoose.disconnect();
-    res.redirect('/');
 }
-})
+
+// Alias check
+function validateAlias(req, res, next) {
+    if (aliasPattern.test(req.body.aliasInput)) {
+        res.status(400).send('Alias contains invalid characters');
+    } else {
+        console.log(2);
+        next();
+    }
+}
+//saving Data
+async function saveData(req, res, next) {
+    try {
+        await mongoose.connect(DB_URL);
+        const oldUrl = await Url.findOne({ url: req.body.urlInput });
+        if (!oldUrl) {
+            let newUrl = new Url({
+                url: req.body.urlInput,
+                alias: req.body.aliasInput
+            });
+            await newUrl.save();
+            mongoose.disconnect();
+            res.redirect('/');
+        } else {
+            console.log(3);
+            next();
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+//updating data
+async function updateData(req ,res){
+    try {
+        await mongoose.connect(DB_URL);
+        const oldUrl = await Url.updateOne({ alias : req.body.aliasInput });
+        mongoose.disconnect();
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+}
 module.exports = router;
+// make alias optional
+// if there is an alias input => convert it into link 
+// if not => make a link 
