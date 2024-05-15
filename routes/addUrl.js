6f51553e2523c784da774c32a2c551390871883b
EyadMohsen <ahmedeyad84@gmail.com>
2024-05-15 17:14:09 +0300

    when there is no alias make a random url

--- routes/addUrl.js
+++ routes/addUrl.js
@@ -9,12 +9,20 @@ const urlPattern = new RegExp('^(https?://)?'+
     '(\\?[;&a-z\\d%_.~+=-]*)?'+
     '(\\#[-a-z\\d_]*)?$','i');
 const aliasPattern = /[^a-zA-Z0-9_]/;
+let alias;
+let count = 0;
+function* uniqueUrlGenerator() {
+    while (true) {
+      const randomString = Math.random().toString(36).substring(2, 8); // Generates a random 6-character string
+    yield `http://example.com/${randomString}`;
+    }
+}
 
 router.post('/',
     validateUrl,
     validateAlias,
     saveData,
-    updateData
+    updateData,
 );
 
 // URL check
@@ -26,51 +34,71 @@ function validateUrl(req, res, next) {
         next();
     }
 }
-
 // Alias check
 function validateAlias(req, res, next) {
-    if (aliasPattern.test(req.body.aliasInput)) {
-        res.status(400).send('Alias contains invalid characters');
-    } else {
-        console.log(2);
+    alias = req.body.aliasInput;
+    if(alias){
+        if (aliasPattern.test(req.body.aliasInput)) {
+            res.status(400).send('Alias contains invalid characters');
+        } else {
+            console.log(2);
+            next();
+        }
+    }
+    else{
+        console.log('no alias')
         next();
     }
 }
 //saving Data
 async function saveData(req, res, next) {
-    try {
-        await mongoose.connect(DB_URL);
-        const oldUrl = await Url.findOne({ url: req.body.urlInput });
-        if (!oldUrl) {
-            let newUrl = new Url({
-                url: req.body.urlInput,
-                alias: req.body.aliasInput
-            });
-            await newUrl.save();
-            mongoose.disconnect();
-            res.redirect('/');
-        } else {
-            console.log(3);
-            next();
+        try {
+            await mongoose.connect(DB_URL);
+            const oldUrl = await Url.findOne({ url: req.body.urlInput });
+            if (!oldUrl) {
+                let newUrl;
+                if(alias){
+                    newUrl = new Url({
+                        url: req.body.urlInput,
+                        alias: req.body.aliasInput
+                    });
+                }
+                else{
+                    const NEW = uniqueUrlGenerator();
+                    console.log(NEW);
+                    newUrl = new Url({
+                        url: req.body.urlInput,
+                        alias: NEW.next().value
+                    });
+                    console.log('no aliaaaaas')
+                }
+                await newUrl.save();
+                mongoose.disconnect();
+                res.redirect('/');
+            } else {
+                console.log(3);
+                next();
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send('Internal Server Error');
         }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-    }
 }
 //updating data
 async function updateData(req ,res){
-    try {
-        await mongoose.connect(DB_URL);
-        const oldUrl = await Url.updateOne({ alias : req.body.aliasInput });
-        mongoose.disconnect();
-        res.redirect('/');
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-    }
+        try {
+            await mongoose.connect(DB_URL);
+            let oldUrl;
+            if(alias){ oldUrl = await Url.updateOne({ alias : req.body.aliasInput }); }   
+            mongoose.disconnect();
+            res.redirect('/');
+        } catch (error) {
+            console.log(error);
+            res.status(500).send('Internal Server Error');
+        }
 }
+
+
 module.exports = router;
-// make alias optional
 // if there is an alias input => convert it into link 
 // if not => make a link 
